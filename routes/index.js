e655ed6703d5a80cd0d6334953747a5153041d1f
iambasanta <sangpangbasanta@gmail.com>
2023-06-25 17:34:20 +0545

    Implement role based authorization

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const AuthRouter = require("./authentication");
+const ProductRouter = require("./product");
 
 router.use("/auth", AuthRouter);
+router.use("/products", ProductRouter);
 
 module.exports = router;
